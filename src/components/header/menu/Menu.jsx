8ff1d90350e2adfe5db8d63371439a9db653d6ea
MIusupova <li.yusupova.01@mail.ru>
2024-03-27 00:43:47 +0100

    в menu добавила ссылки через useState

--- src/components/header/menu/Menu.jsx
+++ src/components/header/menu/Menu.jsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
 import styles from "./Menu.module.css";
 const Menu = () => {
+    const [menu, setMenu] = useState([
+        {
+            "id": "000",
+            "text": "Новинки",
+            "categories": 0
+            },
+            {
+            "id": "001",
+            "text": "Платья",
+            "categories": 6
+            },
+            {
+            "id": "002",
+            "text": "Верх",
+            "categories": 6
+            },
+            {
+            "id": "003",
+            "text": "Юбки",
+            "categories": 5
+            },
+            {
+            "id": "004",
+            "text": "Куртки",
+            "categories": 6
+            },
+            {
+            "id": "005",
+            "text": "Мелочи",
+            "categories": 0
+            },
+            {
+            "id": "006",
+            "text": "Костюмы"
+            }
+    ])
     return (
         <nav className={styles.menu}>
             <ul  className={styles.menuli}>
-                    <li> <a href="" className={styles.list}>Главная</a></li>
-                    <li> <a href="" className={styles.list} >Платья</a></li>
-                    <li> <a href="" className={styles.list} >Верх</a></li>
-                    <li> <a href="" className={styles.list} >Низ</a></li>
-                    <li> <a href="" className={styles.list} >Куртки</a></li>
-                    <li> <a href="" className={styles.list} >Мелочи</a></li>
-                    <li> <a href="" className={styles.list} >Костюмы</a></li>
+                {
+                    menu.map(({id, text}) => <li> <a href="" className={styles.list} key={id}>{text}</a></li>)
+                }
                     <li> <a href="" className={styles.list} >#Boorivagirls</a></li>
                 </ul>        
         </nav>       
