6db77e277a7464bc739cab7e2482d1fc28c01f30
MIusupova <li.yusupova.01@mail.ru>
2024-03-28 22:44:40 +0100

    Реализовала CategoryProduct

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import CategoryProduct from "./pages/categoryProduct/CategoryProduct";
 import HomePage from "./pages/homepage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <HomePage/>
+      {/*<CategoryProduct/>*/}
     </div>
   );
 }
