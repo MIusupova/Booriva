69c297ae49f57424216d35cb4b58e7425d93e99d
Madina-kh16 <madinahacieva007@mail.ru>
2024-04-17 23:35:03 +0300

    changes footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/header/Header";
 import CategoryProduct from "./pages/categoryProduct/CategoryProduct";
 import HomePage from "./pages/homepage/HomePage";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <HomePage/>
+      <Header />
+      <HomePage />
+      <Footer />
       {/*<CategoryProduct/>*/}
     </div>
   );
