06e3b7eece44e75d91b080d411f7f184122ea1d7
MaryamKantaeva <kantaevamaryam25102007@gmail.com>
2024-04-29 19:05:47 +0300

    .

--- src/pages/homepage/categories/categories.jsx
+++ src/pages/homepage/categories/categories.jsx
@@ -1,4 +1,4 @@
-import styles from './categories.module.sass'
+import { Link } from 'react-router-dom'
 import Tshirt from '../../../assets/img/images/category-t-shirt.png'
 import TshirtText from '../../../assets/img/icons/category-t-shirt-text'
 import Bottom from '../../../assets/img/images/category-bottom.png'
@@ -11,24 +11,24 @@ import Costume from '../../../assets/img/images/category-costume.png'
 import CostumeText from '../../../assets/img/icons/category-costume-text'
 import Hoodie from '../../../assets/img/images/category-hoodie.png'
 import HoodieText from '../../../assets/img/icons/category-hoodie-text'
-
+import styles from './categories.module.sass'
 
 const Categories = () => {
     return(
         <div className={styles.categories}>
             <div className={styles.categoriesBox}>
-                <div className={styles.categoriesImgRelative}><img className={styles.imgBig} src={Tshirt} alt='Tshirt' /><div className={`${styles.categoriesImgAbsolute} ${styles.Tshirt}`}><TshirtText/></div></div>
+                <div className={styles.categoriesImgRelative}><img className={styles.imgBig} src={Tshirt} alt='Tshirt' /><div className={`${styles.categoriesImgAbsolute} ${styles.Tshirt}`}><Link to='/categoryProduct'><TshirtText/></Link></div></div>
                 <div className={styles.categoriesBoxSmall}>
-                    <div className={styles.categoriesImgRelative}><img className={styles.imgSmall} src={Bottom} alt='Bottom' /><div className={`${styles.categoriesImgAbsolute} ${styles.Bottom}`}><BottomText/></div></div>
-                    <div className={styles.categoriesImgRelative}><img className={styles.imgSmall} src={Up} alt='Up' /><div className={`${styles.categoriesImgAbsolute} ${styles.Up}`}><UpText/></div></div>
+                    <div className={styles.categoriesImgRelative}><img className={styles.imgSmall} src={Bottom} alt='Bottom' /><div className={`${styles.categoriesImgAbsolute} ${styles.Bottom}`}><Link to='/categoryProduct'><BottomText/></Link></div></div>
+                    <div className={styles.categoriesImgRelative}><img className={styles.imgSmall} src={Up} alt='Up' /><div className={`${styles.categoriesImgAbsolute} ${styles.Up}`}><Link to='/categoryProduct'><UpText/></Link></div></div>
                 </div>
             </div>
             <div className={styles.categoriesBox}>
                 <div className={styles.categoriesBoxSmall}>
-                    <div className={styles.categoriesImgRelative}><img className={styles.imgSmall} src={Dress} alt='Dress' /><div className={`${styles.categoriesImgAbsolute} ${styles.Dress}`}><DressText/></div></div>
-                    <div className={styles.categoriesImgRelative}><img className={styles.imgSmall} src={Costume} alt='Costume' /><div className={`${styles.categoriesImgAbsolute} ${styles.Costume}`}><CostumeText/></div></div>
+                    <div className={styles.categoriesImgRelative}><img className={styles.imgSmall} src={Dress} alt='Dress' /><div className={`${styles.categoriesImgAbsolute} ${styles.Dress}`}><Link to='/categoryProduct'><DressText/></Link></div></div>
+                    <div className={styles.categoriesImgRelative}><img className={styles.imgSmall} src={Costume} alt='Costume' /><div className={`${styles.categoriesImgAbsolute} ${styles.Costume}`}><Link to='/categoryProduct'><CostumeText/></Link></div></div>
                 </div>
-                <div className={styles.categoriesImgRelative}><img className={styles.imgBig} src={Hoodie} alt='Hoodie' /><div className={`${styles.categoriesImgAbsolute} ${styles.Hoodie}`}><HoodieText/></div></div>
+                <div className={styles.categoriesImgRelative}><img className={styles.imgBig} src={Hoodie} alt='Hoodie' /><div className={`${styles.categoriesImgAbsolute} ${styles.Hoodie}`}><Link to='/categoryProduct'><HoodieText/></Link></div></div>
             </div>
         </div>
     )
--- src/pages/homepage/new_collection/new_collection.jsx
+++ src/pages/homepage/new_collection/new_collection.jsx
@@ -1,10 +1,11 @@
-import styles from'./new_collection.module.sass'
+import { Link } from 'react-router-dom'
 import NewCollection from '../../../assets/img/images/newcol.png'
+import styles from'./new_collection.module.sass'
 
 const NewCollectionBox = () => {
     return (
         <div className={styles.new_collection}>
-            <img src={NewCollection} alt='new collection'/>
+            <Link to='/categoryProduct'><img src={NewCollection} alt='new collection'/></Link>
         </div>
     )
 }
