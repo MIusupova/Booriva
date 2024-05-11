4e9a86ad32c934449ab3502268ddf2a424ab588f
MIusupova <li.yusupova.01@mail.ru>
2024-05-12 00:50:53 +0200

    fff

--- src/pages/homepage/ticker/Ticker.jsx
+++ src/pages/homepage/ticker/Ticker.jsx
@@ -1,20 +1,9 @@
 import { Link } from "react-router-dom";
 import styles from "./Ticker.module.sass";
-import { getCatalogAllData } from "../../../services/catalog";
-import { useEffect, useState } from "react";
 
-const Ticker = ({id}) => {
-    const [ticker, setTicker] = useState([])
-    useEffect(() => {  
-        getCatalogAllData().then((res) => setTicker(res));
-        
-    }, [])
-
+const Ticker = () => {
     return (
-        <Link to={`/catalog`}
-        onClick={() => setTicker(id)}
-        key={id}
-         > 
+        <Link to="/catalog">
             <div className = {styles.ticker}>
                 <ul className = {styles.blockticker}>
                     <li className = {styles.text}>топовые товары</li>
