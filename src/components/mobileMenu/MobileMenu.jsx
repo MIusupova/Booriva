3cda9c177a07e4723b9e97533f89e6513544b81c
MIusupova <li.yusupova.01@mail.ru>
2024-09-07 00:06:56 +0200

    MobileMenu

--- src/components/mobileMenu/MobileMenu.jsx
+++ src/components/mobileMenu/MobileMenu.jsx
@@ -1,6 +1,6 @@
 import styles from "./MobileMenu.module.sass";
 
-const MobileMenu = ({title, subtitle}) => {
+const MobileMenu = () => {
     return (
         <nav className={styles.mobileMenu}>
             <div className={styles.mobileMenuItems}>
