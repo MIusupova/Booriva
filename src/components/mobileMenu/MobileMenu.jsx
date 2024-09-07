2253c7dfd547d47664dd571d4d900de7b5f46d28
MIusupova <li.yusupova.01@mail.ru>
2024-09-08 00:35:16 +0200

    fixed mobile menu

--- src/components/mobileMenu/MobileMenu.jsx
+++ src/components/mobileMenu/MobileMenu.jsx
@@ -1,4 +1,7 @@
 import MenuBooriva from "../../assets/img/icons/MenuBooriva";
+import MobileMenuArrow from "../../assets/img/icons/MobileMenuArrow";
+import MobileMenuBasket from "../../assets/img/icons/MobileMenuBasket";
+import MobileMenuHeart from "../../assets/img/icons/MobileMenuHeart";
 import styles from "./MobileMenu.module.sass";
 
 const MobileMenu = () => {
@@ -8,7 +11,17 @@ const MobileMenu = () => {
                 <div className={styles.mobileMenuItem}> <MenuBooriva/>
                     <span className={styles.mobileMenuItemText}>Меню</span>
                 </div>
+                <div className={styles.mobileMenuItem}> <MobileMenuArrow/>
+                    <span className={styles.mobileMenuItemText}>Написать</span>
+                </div>
+                <div className={styles.mobileMenuItem}> <MobileMenuBasket/>
+                    <span className={styles.mobileMenuItemText}>Корзина</span>
+                </div>
+                <div className={styles.mobileMenuItem}> <MobileMenuHeart/>
+                    <span className={styles.mobileMenuItemText}>Wishlist</span>
+                </div>
                 
+
             </div>
 
             
