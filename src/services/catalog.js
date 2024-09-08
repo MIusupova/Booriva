3bd2438311eb26eaadb1602c944b1d74cb7668fb
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-09-08 21:03:10 +0300

    fix: catalog

--- src/services/catalog.js
+++ src/services/catalog.js
@@ -19,3 +19,15 @@ export const getCategoryData = async (Id) => {
   return res;
 };
 
+export const getCatalogDataNew = async (id) => {
+  let res = await fetch(
+    `https://65588446e93ca47020a966c9.mockapi.io/menuCatalog?menuId=000`
+  );
+
+  if (res.ok) {
+    res = await res.json();
+    return res[0];
+  } else {
+    return [];
+  }
+};
