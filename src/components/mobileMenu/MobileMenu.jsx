bf42c706d466a6c81f8058c131ba20bb6453dc7a
MIusupova <li.yusupova.01@mail.ru>
2024-09-21 23:45:24 +0200

    изменения на верстке

--- src/components/mobileMenu/MobileMenu.jsx
+++ src/components/mobileMenu/MobileMenu.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import MenuBooriva from "../../assets/img/icons/MenuBooriva";
 import MobileMenuArrow from "../../assets/img/icons/MobileMenuArrow";
 import MobileMenuBasket from "../../assets/img/icons/MobileMenuBasket";
@@ -5,6 +6,7 @@ import MobileMenuHeart from "../../assets/img/icons/MobileMenuHeart";
 import styles from "./MobileMenu.module.sass";
 
 const MobileMenu = ({isBasketOpen, setIsBasketOpen}) => {
+    const allProductBasket = useSelector(state => state.cart.allProductBasket)
     return (
         <nav className={styles.mobileMenu}>
             <div className={styles.mobileMenuItems}>
@@ -16,6 +18,7 @@ const MobileMenu = ({isBasketOpen, setIsBasketOpen}) => {
                 </div>
                 <div className={styles.mobileMenuItem} isBasketOpen={isBasketOpen} setIsBasketOpen={setIsBasketOpen} onClick={()=> setIsBasketOpen(true)}> <MobileMenuBasket/>
                     <span className={styles.mobileMenuItemText}>Корзина</span>
+                    <div className={styles.allSumBasket}>{allProductBasket}</div>
                 </div>
                 <div className={styles.mobileMenuItem}> <MobileMenuHeart/>
                     <span className={styles.mobileMenuItemText}>Wishlist</span>
