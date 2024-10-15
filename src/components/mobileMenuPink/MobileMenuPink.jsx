bf5f5bed96f50b228c5527093f7cf348c5701dcf
MIusupova <li.yusupova.01@mail.ru>
2024-10-16 00:53:30 +0200

    menuPink

--- src/components/mobileMenuPink/MobileMenuPink.jsx
+++ src/components/mobileMenuPink/MobileMenuPink.jsx
@@ -1,22 +1,32 @@
 
+import { Link } from "react-router-dom";
 import CrossButtonBasketPink from "../../assets/img/icons/CrossButtonBasketPink";
 import styles from "./MobileMenuPink.module.sass";
+import { getMenuItems } from "../../services/menu";
+import { useEffect, useState } from "react";
 const MobileMenuPink = ({ isMobileMenuOpen,
     setMobileMenuOpen}) => {
+        const [menu, setMenu] = useState([]);
+        useEffect(() => {
+            getMenuItems().then((res) => setMenu(res));
+  }, []);
     return (
       <div className={styles.mobileMenuPink  + " " + (isMobileMenuOpen && styles.mobileMenuPink_open)}>
         <CrossButtonBasketPink setMobileMenuOpen={setMobileMenuOpen}/>
         <div className={styles.mobileMenuBlock} >
         <div className={styles.mobileMenuText}>Меню</div>
         <ul className={styles.mobileMenuTextBlox}>
-            <li className={styles.mobileMenuTextLi}>Новинки</li>
-            <li className={styles.mobileMenuTextLi}>платья</li>
-            <li className={styles.mobileMenuTextLi}>верх</li>
-            <li className={styles.mobileMenuTextLi}>юбки</li>
-            <li className={styles.mobileMenuTextLi}>верхняя одежда</li>
-            <li className={styles.mobileMenuTextLi}>мелочи</li>
-            <li className={styles.mobileMenuTextLi}>костюмы</li>
-            <li className={styles.mobileMenuTextLi}>#Boorivagirls</li>
+            {menu.map(({ id, name }) => (
+          
+             <Link to={`/booriva/catalog?menuId=${id}`}><li className={styles.mobileMenuTextLi} key={id}>{name}</li></Link>
+            
+                
+            
+        ))}
+
+            <Link to="/booriva/boorivagirls" className={styles.mobileMenuTextLi}>
+                #Boorivagirls
+            </Link>
         </ul>
 
 
