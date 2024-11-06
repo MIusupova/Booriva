fd9de4f50ed56eb8a8531e20c09e365c99e793fe
MIusupova <li.yusupova.01@mail.ru>
2024-11-06 01:38:16 +0100

    booriva store

--- src/pages/homepage/boorivaStore/Subtitle/subtitle.jsx
+++ src/pages/homepage/boorivaStore/Subtitle/subtitle.jsx
@@ -3,7 +3,7 @@ import styles from "./subtitle.module.sass";
 const Subtitle = () => {
   return (
     <div className={styles.text}>
-      г.Грозн, ул.Кадыр., Ежедн. с 11:00 до 21:00
+      г. Москва, пер. Арбат 21. с 11:00 до 21:00
     </div>
   );
 };
